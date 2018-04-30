66467ef43f9749632506e929b3d0ec5b361d1622
supernhok13 <huytranle13@gmail.com>
2018-04-30 15:52:49 +0700

    Fix bug and Fix Field Template Image and Action

--- CoffeeHome/ViewModel/HomeViewModel.cs
+++ CoffeeHome/ViewModel/HomeViewModel.cs
@@ -16,14 +16,14 @@ namespace CoffeeHome.ViewModel
         public HomeViewModel()
         {
             this.Items = new ObservableCollection<ItemField>(){
-                  new ItemField("Đồ uống",this.template,new FoodDrinkTemplate(),"Images/drink-food.png"),
-                  new ItemField("Loại đồ uống",this.template,new DrinkTypeTemplate(),"Images/drink-type.png"),
-                  new ItemField("Thành viên",this.template,new CustomerTemplate(),"Images/customer.png"),
-                  new ItemField("Bàn",this.template,new TableTemplate(),"Images/table.png"),
-                  new ItemField("Hóa đơn",this.template,new BillTemplate(),"Images/bill.png"),
-                  new ItemField("Chi tiết Hóa đơn",this.template,new BillDetailTemplate(),"Images/bill_detail.png"),
-                  new ItemField("Hóa đơn Online",this.template,new BillOnlineTemplate(),"Images/bill_online.png"),
-                  new ItemField("Chi tiết HĐ Online",this.template,new BillOnlineDetailTemplate(),"Images/bill_online_detail.png")
+                  new ItemField("Đồ uống",this.template,new FoodDrinkTemplate(),"Asset/Images/drink-food.png"),
+                  new ItemField("Loại đồ uống",this.template,new DrinkTypeTemplate(),"Asset/Images/drink-type.png"),
+                  new ItemField("Thành viên",this.template,new CustomerTemplate(),"Asset/Images/customer.png"),
+                  new ItemField("Bàn",this.template,new TableTemplate(),"Asset/Images/table.png"),
+                  new ItemField("Hóa đơn",this.template,new BillTemplate(),"Asset/Images/bill.png"),
+                  new ItemField("Chi tiết Hóa đơn",this.template,new BillDetailTemplate(),"Asset/Images/bill_detail.png"),
+                  new ItemField("Hóa đơn Online",this.template,new BillOnlineTemplate(),"Asset/Images/bill_online.png"),
+                  new ItemField("Chi tiết HĐ Online",this.template,new BillOnlineDetailTemplate(),"Asset/Images/bill_online_detail.png")
             };
         }
     }
